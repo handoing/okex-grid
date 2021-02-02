4376535beae1b404760d625e280bcb32b49d0ef3
chenhan1 <chenhan1@360.cn>
2021-02-02 20:44:19 +0800

    update

--- bootstrap.js
+++ bootstrap.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 const OkexGrid = require('./src');
 
 function sendDD(content) {
+  // 钉钉机器人hook
 	const url = 'https://oapi.dingtalk.com/robot/send?access_token=d2c4f05b48ca1fa74d4b6b1d8d9f647644de2f129658719003e695affdc84c09';
 	axios.post(url, {
 		msgtype: "text",
---
+++ plopfile.js
@@ -0,0 +1,29 @@
+module.exports = function (plop) {
+  plop.setGenerator('init', {
+    description: 'init user.json',
+    prompts: [
+      {
+        type: 'append',
+        name: 'apiKey',
+        message: 'Please input api key: '
+      },
+      {
+        type: 'append',
+        name: 'secretKey',
+        message: 'Please input secret key: '
+      },
+      {
+        type: 'append',
+        name: 'passphrase',
+        message: 'Please input passphrase: '
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: 'user.json',
+        templateFile: 'plop/user.plop'
+      }
+    ]
+});
+};
